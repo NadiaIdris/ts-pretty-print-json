bdbc02d46c2e7b6193729619f178e290fb6745cc
Nadia Idris <nadia@fasterlap.com>
2022-11-21 11:42:17 -0800

    Create custom components

---
+++ src/components/KeyValueWrapper.tsx
@@ -0,0 +1,39 @@
+interface KeyValueWrapperProps {
+  keyProp: string;
+  onClickHandler: (
+    key: string,
+    isOpen: Map<string, boolean>,
+    setIsOpen: (isOpen: Map<string, boolean>) => void
+  ) => void;
+  isOpen: Map<string, boolean>;
+  setIsOpen: (isOpen: Map<string, boolean>) => void;
+  children: React.ReactNode;
+  type: "object" | "array";
+}
+
+const KeyValueWrapper = ({
+  keyProp,
+  onClickHandler,
+  isOpen,
+  setIsOpen,
+  children,
+  type,
+}: KeyValueWrapperProps) => {
+  const bracket = type === "array" ? ["[", "]"] : ["{", "}"];
+  return (
+    <div key={keyProp} style={{ marginLeft: "20px" }}>
+      {`"${keyProp}":`}
+      <span
+        style={{ cursor: "pointer" }}
+        onClick={() => onClickHandler(keyProp, isOpen, setIsOpen)}
+      >
+        {isOpen.get(keyProp) ? "▼ " : "▶ "}
+      </span>
+      {bracket[0]}
+      {children}
+      {bracket[1]}
+    </div>
+  );
+};
+
+export default KeyValueWrapper;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { data } from './data';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { data } from "./data";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App data={data}/>
+    <App data={data} />
   </React.StrictMode>
 );
 
